38de6c2edfb4927edc9886415f6664a133b48971
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2019-08-29 17:39:31 +0200

    Add items reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,21 @@
+function itemsReducer(state, value) {
+    const { action, payload } = value
+    switch (action) {
+        case "set": {
+            return { ...state, [payload.id]: payload }
+        }
+        case "unset": {
+            const { [payload.id]: deleted, ...updatedState } = state
+            return updatedState
+        }
+        default:
+            return state
+    }
+}
+
 export default {
-    loading: {
-        state: false,
-        reducer: (state, value) => value,
+    items: {
+        state: {},
+        reducer: itemsReducer,
     },
 }
